bea2c9fe605b13711f4cec95b4c629632ff536b0
sinsehwan <sk124590@gmail.com>
2026-02-10 15:21:40 +0900

    feat(controller): implement basic lottoController

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,10 +3,11 @@ package lotto.view;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
 public class OutputView {
 
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8));
 
     public void write(OutputMessage outputMessage, Object... args) throws IOException {
         bw.write(outputMessage.format(args));
