afa89c508aaa6a575474ac9874d000fe3fe7dd4b
kakao-daniel-hoon <daniel.hoon@kakaocorp.com>
2026-02-11 12:26:55 +0900

    refactor: Rank의 당첨 금액 및 등수 정책을 도메인 내부로 이동

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,11 +3,11 @@ package lotto.domain;
 public enum Rank {
 
     MISS(0, 0),
-    FIFTH(3, 5_000),
+    FIFTH(5, 5_000),
     FOURTH(4, 50_000),
-    THIRD(5, 1_500_000),
-    SECOND(5, 30_000_000),
-    FIRST(6, 2_000_000_000);
+    THIRD(3, 1_500_000),
+    SECOND(2, 30_000_000),
+    FIRST(1, 2_000_000_000);
 
     private final int ranking;
     private final int winningMoney;
