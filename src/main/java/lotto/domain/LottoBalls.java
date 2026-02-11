fb7414729f8559fcf0a27b805e091acc5de6ee6c
sinsehwan <sk124590@gmail.com>
2026-02-11 16:40:35 +0900

    chore: apply simple grammar

--- src/main/java/lotto/domain/LottoBalls.java
+++ src/main/java/lotto/domain/LottoBalls.java
@@ -23,7 +23,7 @@ public class LottoBalls {
     }
 
     private static void checkLottoLength(Set<Integer> numSet) {
-        if (!(numSet.size() == LOTTO_LENGTH)) {
+        if (numSet.size() != LOTTO_LENGTH) {
             throw new LottoException(ExceptionCode.INVALID_LOTTO_NUMBER_COUNT);
         }
     }
