dcd3f54310ac51b73b1c7100d0406a3ddd45ccc1
kakao-daniel-hoon <daniel.hoon@kakaocorp.com>
2026-02-12 14:45:46 +0900

    chore: 자바 컨벤션 유지 - Lotto 클래스 내 상수를 최상단으로 이동

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,8 +3,8 @@ package lotto.domain;
 import lotto.util.LottoAutoCreator;
 
 public class Lotto {
-    private final LottoBalls lottoBalls;
     private static final int PRICE = 1000;
+    private final LottoBalls lottoBalls;
 
     public Lotto() {
         this.lottoBalls = LottoAutoCreator.lottoAutoCreate();
